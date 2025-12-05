8ce9bdfdbfb6b53574517af14bb9e52e5e5473aa
Monsef Mselhy <127881154+MonseF50@users.noreply.github.com>
2025-12-05 19:51:04 +0200

    structure of the project build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent), title: ' 😊 Home' },
+    { path: 'about', loadComponent: () => import('./components/about/about.component').then(c => c.AboutComponent), title: 'About' },
+    { path: 'about', loadComponent: () => import('./components/gallery/gallery.component').then(c => c.GalleryComponent), title: 'gallery' },
+    { path: 'about', loadComponent: () => import('./components/contact/contact.component').then(c => c.ContactComponent), title: 'contact' },
+    { path: '**', loadComponent: () => import('./components/not-founded/not-founded.component').then(c => c.NotFoundedComponent), title: 'Not Founded' }
+];
---
+++ src/app/components/not-founded/not-founded.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-founded',
+  imports: [],
+  templateUrl: './not-founded.component.html',
+  styleUrl: './not-founded.component.scss'
+})
+export class NotFoundedComponent {
+
+}
